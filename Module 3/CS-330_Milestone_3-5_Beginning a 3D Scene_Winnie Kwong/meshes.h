a578f049bcfc8c480d2b773aa3c69ed07c5a99cf
ohxmisskay <130874058+ohxmisskay@users.noreply.github.com>
2024-09-18 15:14:55 -0700

    Module 3 Assignment

---
+++ Module 3/CS-330_Milestone_3-5_Beginning a 3D Scene_Winnie Kwong/meshes.h
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+// meshes.h
+// ========
+// create meshes for various 3D primitives: plane, cylinder, torus, sphere
+//
+//  meshes.h code provided by: Brian Battersby - SNHU Instructor / Computer Science
+//	Created for CS-330-Computational Graphics and Visualization, Nov. 7th, 2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+
+class Meshes
+{
+
+public:
+
+	// Stores the GL data relative to a given mesh
+	struct GLMesh
+	{
+		GLuint vao;         // Handle for the vertex array object
+		GLuint vbos[2];     // Handles for the vertex buffer objects
+		GLuint nVertices;	// Number of vertices for the mesh
+		GLuint nIndices;    // Number of indices for the mesh
+	};
+
+	GLMesh gCylinderMesh;
+	GLMesh gPlaneMesh;
+	GLMesh gSphereMesh;
+	GLMesh gTorusMesh;
+
+public:
+	void CreateMeshes();
+	void DestroyMeshes();
+
+private:
+	void UCreatePlaneMesh(GLMesh& mesh);
+	void UCreateCylinderMesh(GLMesh& mesh);
+	void UCreateTorusMesh(GLMesh& mesh);
+	void UCreateSphereMesh(GLMesh& mesh);
+
+	void UDestroyMesh(GLMesh& mesh);
+
+};
