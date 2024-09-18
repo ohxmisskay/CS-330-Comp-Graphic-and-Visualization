700bf135ddba8f10b221df4faec9a398a688b41f
ohxmisskay <130874058+ohxmisskay@users.noreply.github.com>
2024-09-18 15:43:41 -0700

    Module 6 Assignment

---
+++ Module 6/6-5 Milestone Lighting Complex Objects_Winnie Kwong/Mesh.h
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+// mesh.h
+// ========
+// create meshes for various 3D primitives: plane, cylinder, torus, sphere
+//
+//  meshes.h code provided by: Brian Battersby - SNHU Instructor / Computer Science
+//	Created for CS-330-Computational Graphics and Visualization, Nov. 7th, 2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+
+class Meshes
+{
+
+public:
+
+	// Stores the GL data relative to a given mesh
+	struct GLMesh
+	{
+		GLuint vao;         // Handle for the vertex array object
+		GLuint vbos[2];     // Handles for the vertex buffer objects
+		GLuint nVertices;	// Number of vertices for the mesh
+		GLuint nIndices;    // Number of indices for the mesh
+	};
+
+	GLMesh gCylinderMesh;
+	GLMesh gPlaneMesh;
+	GLMesh gSphereMesh;
+	GLMesh gTorusMesh;
+	GLMesh gBoxMesh;
+
+public:
+	void CreateMeshes();
+	void DestroyMeshes();
+
+private:
+	void UCreatePlaneMesh(GLMesh& mesh);
+	void UCreateCylinderMesh(GLMesh& mesh);
+	void UCreateTorusMesh(GLMesh& mesh);
+	void UCreateSphereMesh(GLMesh& mesh);
+	void UCreateBoxMesh(GLMesh& mesh);
+	void UDestroyMesh(GLMesh& mesh);
+
+
+};
